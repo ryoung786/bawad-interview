7585a18111404f0cd5d0aaa789bb78d73e7fb900
Ryan Young <ryoung786@gmail.com>
2021-06-15 10:59:59 -0400

    bonus 1: toggle sort order between asc, desc, AND unsorted

--- lib/bawad_web/live/page_live.ex
+++ lib/bawad_web/live/page_live.ex
@@ -2,16 +2,19 @@ defmodule BawadWeb.PageLive do
   use BawadWeb, :live_view
 
   @fields ~w(name city country state street_name street_number)
-  @dirs ~w(asc desc)
+  @dirs ~w(asc desc unsorted)
 
   @impl true
   def mount(_params, _session, socket) do
+    users = fetch_users()
+
     {:ok,
      assign(socket,
        query: "",
        results: %{},
-       users: fetch_users(),
-       sort: %{field: :name, order: :asc}
+       unsorted_users: users,
+       users: users,
+       sort: %{field: :name, order: :unsorted}
      )}
   end
 
@@ -23,27 +26,31 @@ defmodule BawadWeb.PageLive do
     {:noreply,
      socket
      |> assign(sort: %{field: field, order: order})
-     |> assign(users: sort_users(socket.assigns.users, field, order))}
+     |> assign(users: sort_users(socket.assigns.unsorted_users, field, order))}
   end
 
   @impl true
   def handle_event("sort", %{"field" => field}, socket) do
     dir =
       if field == Atom.to_string(socket.assigns.sort.field),
-        do: flip_dir(socket.assigns.sort.order),
-        else: :asc
+        do: next_sort_order(socket.assigns.sort.order),
+        else: next_sort_order(:unsorted)
 
     url = BawadWeb.Router.Helpers.page_path(socket, :index, %{sort: field, dir: dir})
     {:noreply, push_patch(socket, to: url)}
   end
 
-  defp flip_dir(:asc), do: :desc
-  defp flip_dir(:desc), do: :asc
+  defp next_sort_order(:unsorted), do: :asc
+  defp next_sort_order(:asc), do: :desc
+  defp next_sort_order(:desc), do: :unsorted
+
   defp sanitize_sort_field_param(param, _default) when param in @fields, do: String.to_atom(param)
   defp sanitize_sort_field_param(_param, default), do: default
 
   defp sanitize_sort_order_param(param, _default) when param in @dirs, do: String.to_atom(param)
   defp sanitize_sort_order_param(_param, default), do: default
+
+  defp sort_users(users, _field, :unsorted), do: users
 
   defp sort_users(users, field, dir) do
     Enum.sort_by(users, fn user -> user[field] end, dir)
@@ -65,13 +72,15 @@ defmodule BawadWeb.PageLive do
     end)
   end
 
-  def th(field, sort) do
+  def th(field, %{field: sort_field, order: sort_order}) do
     assigns = %{
       value: Atom.to_string(field),
       field_name: field |> Atom.to_string() |> String.capitalize() |> String.replace("_", " "),
       arrow:
-        if field == sort.field do
-          if sort.order == :asc, do: "▼", else: "▲"
+        case {field, sort_order} do
+          {^sort_field, :asc} -> "▼"
+          {^sort_field, :desc} -> "▲"
+          _ -> ""
         end
     }
 
